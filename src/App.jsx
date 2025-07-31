e2b038d6c939a9aaf232155556a2f0bcff554285
esha21khalid <eshakhalid078@gmail.com>
2025-07-31 12:42:40 +0500

    Updated heading in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ function App() {
   return (
    <>
    <h1>esha</h1>
+   <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium minima ducimus reprehenderit nesciunt sit. Eligendi voluptatem recusandae rem harum molestias? Provident perspiciatis et totam aliquam nobis id incidunt repellat quas?
+   </p>
    </>
   )
 }
